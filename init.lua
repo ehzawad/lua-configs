7230e877f2cbfbe92a2e080e80fff3cf52df61cf
Emrul Hasan Zawad <ehzawad@gmail.com>
2023-07-30 11:41:32 +0600

    debugger

--- init.lua
+++ init.lua
@@ -240,8 +240,8 @@ require('lazy').setup({
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open({ reset = true })
       end
-      dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-      dap.listeners.before.event_exited["dapui_config"] = dapui.close
+      -- dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+      -- dap.listeners.before.event_exited["dapui_config"] = dapui.close
     end
   },
 
