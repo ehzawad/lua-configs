8137a91aa8a2391d99601fc1de0ce4e38d603c87
ehzawad <ehzawad@gmail.com>
2024-05-31 19:42:12 +0600

    removed symbol outline

--- init.lua
+++ init.lua
@@ -67,7 +67,6 @@ require('lazy').setup({
   },
 
   { "catppuccin/nvim",         name = "catppuccin", priority = 1000 },
-  { "simrat39/symbols-outline.nvim" },
 
   {
     -- Autocompletion
@@ -303,13 +302,6 @@ require('nvim-treesitter.configs').setup {
     },
   },
 }
-
-require("symbols-outline").setup()
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
